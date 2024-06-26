5dd126f20e5747a2b0ef2855cf2bd353fb1fbfdc
Your Name <you@example.com>
2024-06-26 11:57:59 +0200

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:oukso/firebase_options.dart';
 import 'package:oukso/src/app.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
